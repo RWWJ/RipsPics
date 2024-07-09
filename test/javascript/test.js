eb6a3c41f4ab407d00544566e3a39cd5f4843aff
Ray Wallace <HoudiniJr@GMail.com>
2024-07-09 14:50:46 -0400

    Test misc

--- test/javascript/test.js
+++ test/javascript/test.js
@@ -10,13 +10,21 @@
 doTest( )
 
 function doTest( ) {
-  TestImageID.addEventListener( "mousemove", event => {
+  // let can = document.createElement( "canvas" )
+  let ctx = TestCanvasID.getContext( "2d" )
+  TestCanvasID.width =  100
+  TestCanvasID.height = 100
+
+  ctx.fillRect( 0, 20, 20, 20 )
+
+  TestCanvasID.addEventListener( "mousemove", event => {
     TestAreaID.innerHTML += `event: ${JSON.stringify(event)} <br><hr>`
     TestAreaID.innerHTML += `event.offsetX: ${event.offsetX} <br><hr>`
     TestAreaID.innerHTML += `event.clientX: ${event.clientX} <br><hr>`
     TestAreaID.innerHTML += `event.pageX: ${event.pageX} <br><hr>`
+    TestAreaID.innerHTML += `Calculated: ${event.pageX - event.target.offsetLeft - event.target.clientLeft} <br><hr>`
   } )
-  TestImageID.addEventListener( "touchmove", event => {
+  TestCanvasID.addEventListener( "touchmove", event => {
     TestAreaID.innerHTML += `event.changedTouches: ${JSON.stringify(event.changedTouches[0])} <br><hr>`
     TestAreaID.innerHTML += `.changedTouches.offsetX: ${event.changedTouches[0].offsetX} <br><hr>`
     TestAreaID.innerHTML += `.changedTouches.clientX: ${event.changedTouches[0].clientX} <br><hr>`
